c6516dc3990a589cffa5a047e625f4f70b983722
Ammarhr <hariry49@gmail.com>
2021-06-23 12:11:24 +0300

    add sum function from Ammar

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 'use strict';
 let num1 = Number(prompt('enter num1'));
 let num2 = Number(prompt('enter num2'));
-let multi = num1 + num2;
+// let multi = num1 + num2;
+
+function sum(x, y) {
+    let sum = x + y;
+    console.log(sum);
+}
+
+sum(num1, num2);
